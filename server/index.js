1e35a0646b9a735322694ed504352d12ca62f7f0
Sana-Amin <amin.sana19@gmail.com>
2022-07-27 14:11:39 -0500

    second commit

--- server/index.js
+++ server/index.js
@@ -9,6 +9,14 @@ app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
+
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/styles.css'))
+})
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/main.js'))
+})
+
 const port = process.env.PORT || 4005
 
 app.listen(port, () => {console.log(`Listening on port ${port}`)})
