37facc9102332169e6af1929162cacbec6762bab
Sana-Amin <amin.sana19@gmail.com>
2022-07-27 14:54:38 -0500

    fifth commit

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.get('/', function(req, res){
 
 
 app.get('/css', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/styles.css'))
+    res.sendFile(path.join(__dirname, '../public/style.css'))
 })
 app.get('/js', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/main.js'))
